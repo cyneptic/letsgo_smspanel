c760c002032dc52290e9363b29fd123f36f36c3a
Alireza <50639733+cyneptic@users.noreply.github.com>
2023-06-29 21:53:22 +0330

    Update contact.go
    adds UserID to Contact struct

--- internal/core/entities/contact.go
+++ internal/core/entities/contact.go
@@ -4,6 +4,7 @@ import "github.com/google/uuid"
 
 type Contact struct {
 	DBModel
+	UserID      uuid.UUID `json:"user_id"`
 	PhoneBookID uuid.UUID `json:"phone_book_id"`
 	FirstName   string    `gorm:"size:255" json:"first_name"`
 	LastName    string    `gorm:"size:255" json:"last_name"`
