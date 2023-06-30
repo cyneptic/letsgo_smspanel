e6e2b2a1e280d434f566c7777ca5467aa973c5d5
Amir Dashti <ahdashti@gmail.com>
2023-06-30 20:34:02 +0330

    feat: add custom logger middleware

--- cmd/main.go
+++ cmd/main.go
@@ -4,11 +4,13 @@ import (
 	"log"
 
 	controllers "github.com/cyneptic/letsgo-smspanel/controller"
+	"github.com/cyneptic/letsgo-smspanel/controller/middleware"
 	"github.com/labstack/echo/v4"
 )
 
 func main() {
 	e := echo.New()
+	e.Use(middleware.CustomLogger)
 	controllers.AddPhoneBookRoutes(e)
 	controllers.AddContactRoutes(e)
 
