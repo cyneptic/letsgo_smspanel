fa161af337dc9adf853f3c259719b4749e345c2a
amir62hossein <amirnourian378@gmail.com>
2023-07-03 21:11:02 +0330

    review done

--- internal/core/service/wallet_service.go
+++ internal/core/service/wallet_service.go
@@ -12,46 +12,47 @@ import (
 type WalletService struct {
 	db ports.WalletRepositoryContracts
 }
+
 func NewWalletService() *WalletService {
 	db := repositories.NewGormDatabase()
 	return &WalletService{
 		db: db,
 	}
 }
-func (r *WalletService) GenerateWalletService(userID uuid.UUID) error  {
+func (r *WalletService) GenerateWalletService(userID uuid.UUID) error {
 	var generatedWallet entities.Wallet
 	generatedWallet.UserID = userID
 	generatedWallet.CreatedAt = time.Now()
 	generatedWallet.Credit = 0
-	err := r.db.GenerateWalletRepository(generatedWallet)
+	err := r.db.GenerateWallet(generatedWallet)
 	if err != nil {
 		return err
 	}
 	return nil
 }
-func (r *WalletService) ChargeWalletService(walletID, userID uuid.UUID , amount int) error  {
+func (r *WalletService) ChargeWalletService(walletID, userID uuid.UUID, amount int) error {
 
-	 err := r.db.ChargeWalletRepository(walletID , userID , amount)
+	err := r.db.ChargeWallet(walletID, userID, amount)
 	return err
 }
 func (r *WalletService) GetWalletAmountService(walletID, userID uuid.UUID) (int, error) {
-	wallet , err := r.db.GetWalletAmountRepository(walletID)
+	wallet, err := r.db.GetWallet(walletID)
 	if err != nil {
-		return 0 , err
+		return 0, err
 	}
 	if wallet.UserID != userID {
 		err := errors.New("userId is not match with this walletId")
 		return 0, err
 	}
-	return wallet.Credit , nil
+	return wallet.Credit, nil
 }
-func (r *WalletService ) HasEnoughCreditService(walletID uuid.UUID, amount int) (bool, error)  {
-	wallet , err := r.db.HasEnoughCreditRepository(walletID)
+func (r *WalletService) HasEnoughCreditService(walletID uuid.UUID, amount int) (bool, error) {
+	wallet, err := r.db.GetWallet(walletID)
 	if err != nil {
 		return false, err
 	}
 	if wallet.Credit >= amount {
-		return true , nil
+		return true, nil
 	}
-	return false , nil
+	return false, nil
 }
