cca5972f6b099ed020048fe165f44585e70fb187
Alima <aliali.ali1378@yahoo.com>
2023-07-09 21:51:23 +0330

    final model for number

--- internal/core/entities/number.go
+++ internal/core/entities/number.go
@@ -1,38 +1,17 @@
 package entities
 
 import (
-	"database/sql/driver"
 	"time"
 
 	"github.com/google/uuid"
 )
 
-type NumberType string
-
-func (nt *NumberType) Scan(value interface{}) error {
-	*nt = NumberType(value.([]byte))
-	return nil
-}
-
-func (nt NumberType) Value() (driver.Value, error) {
-	return string(nt), nil
-}
-
-const (
-	SHARED     NumberType = "Shared"
-	SUBSCRIBED            = "Subscribed"
-	BOUGHT                = "Bought"
-)
-
 type Number struct {
 	DBModel
-	No   string     `gorm:"Column:number" json:"number"`
-	Type NumberType `gorm:"type:number_type"`
-}
-
-type UserNumbers struct {
-	DBModel
-	UserId           uuid.UUID `gorm:"type:uuid" json:"user_id"`
-	Number           uuid.UUID `gorm:"type:uuid" json:"number"`
+	No               string    `gorm:"Column:number" json:"number"`
+	UserId           uuid.UUID `gorm:"type:uuid" json:"user_id"` // shared number owned to lets_go
+	IsShared         bool      `gorm:"Column:is_shared"`
+	Subscribed       bool      `gorm:"Column:subscribed"`
+	IsActive         bool      `gorm:"Column:is_active"`
 	SubscriptionDate time.Time `json:"subscription_date"`
 }
